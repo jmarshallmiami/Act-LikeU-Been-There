3f3ddd4e7fc488ce797d511e6fdc1761386c2e60
Ambrose Shokunbi <shokunbitobi@gmail.com>
2022-04-12 23:23:05 -0500

    need to pull updated files

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,21 +3,9 @@ var teamSeasonGames = "https://www.balldontlie.io/api/v1/games?seasons[]=1979&te
 var selectTeamObject = document.querySelector(".team-select");
 var submitButton = document.querySelector("section button");
 
-<<<<<<< HEAD
 var submitHandler = function(event) {
   event.preventDefault();
 }
-=======
-// //fetch all NBA team names from balldontlie
-// fetch(teamNames).then(function (response) {
-//   // request was successful
-//   if (response.ok) {
-//     return response.json();
-//   }
-// })
-//   .then(function (data) {
-//     var teamData = data.data;
->>>>>>> c93075972862f86252d2c1573d8ebbeb48dadfbc
 
 //     for (var i = 0; i < teamData.length; i++) {
 //       var teamNamesEl = document.createElement("option");
