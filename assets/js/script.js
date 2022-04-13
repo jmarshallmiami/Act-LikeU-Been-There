0943e38cebf097add6968478c3a5dffa3f0d38e1
Jules <j.marshallmiami@yahoo.com>
2022-04-12 21:41:51 -0500

    working on correcting API call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,44 @@
-var team = {};
 var teamNames = "https://www.balldontlie.io/api/v1/teams";
-var seasonGames = "https://www.balldontlie.io/api/v1/games?seasons[]=2018&team_ids[]=6&per_page=100";
+var teamSeasonGames = "https://www.balldontlie.io/api/v1/games?seasons[]=1979&team_ids[]=3"
 var selectTeamObject = document.querySelector(".team-select");
 var submitButton = document.querySelector("section button");
 
+// //fetch all NBA team names from balldontlie
+// fetch(teamNames).then(function (response) {
+//   // request was successful
+//   if (response.ok) {
+//     return response.json();
+//   }
+// })
+//   .then(function (data) {
+//     var teamData = data.data;
 
+//     for (var i = 0; i < teamData.length; i++) {
+//       var teamNamesEl = document.createElement("option");
+//       var teamID = teamData[i].id;
+//       teamNamesEl.setAttribute("value", teamID);
+//       teamNamesEl.innerHTML = teamData[i].full_name
+//       selectTeamObject.appendChild(teamNamesEl)
+//     };
+//   });
 
-
-//fetch all NBA team names from balldontlie
-fetch(teamNames).then(function (response) {
-  // request was successful
-  if (response.ok) {
-    return response.json();
-  }
-})
+//fetch all available years names from balldontlie
+fetch(teamSeasonGames)
+  .then(function (response) {
+    // request was successful
+    if (response.ok) {
+      return response.json();
+    }
+  })
   .then(function (data) {
-    var team = data.data;
-
-    for (var i = 0; i < team.length; i++) {
-      var teamNamesEl = document.createElement("option");
-      var teamID = team[i].id;
-      teamNamesEl.setAttribute("value", teamID);
-      teamNamesEl.innerHTML = team[i].full_name
-      selectTeamObject.appendChild(teamNamesEl)
-    };
-
+    console.log(data.data);
   });
 
-// choose team function
-var chooseTeam = function () {
-  var teamSelected = document.querySelector("option").textContent;
-  console.log(teamSelected);
-};
 
 // create clickable select elements that triggers functions to pull years in the playoff and championships
-submitButton.addEventListener("click", chooseTeam)
+// submitButton.addEventListener("click", chooseTeam)
+
+// FUNCTION: retreive team name and id from select HTML element options
+
+// FUNCTION: 
 
